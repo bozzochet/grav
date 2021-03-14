a99597b5223ceecdada57e31e23d1a8471dfd43a
Matteo Duranti <bozzo1983@gmail.com>
2021-03-14 18:36:41 +0100

    up

--- remove_plugins.sh
+++ remove_plugins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 
-#this on the final server should be removed
-#bin/gpm uninstall -y admin
-
 bin/gpm uninstall -y facebook-feed
+
+#this on the final server should be removed
+bin/gpm uninstall -y admin
+
