167b127685200c37909d8b72c24c3b7b3395f7b9
Matteo Duranti <bozzo1983@gmail.com>
2021-05-06 17:24:05 +0200

    up

---
+++ update_bibtex.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "---" > text.md
+echo "title: Publications" >> text.md
+echo "body_classes: modular" >> text.md
+echo "features:" >> text.md
+echo "visible: false" >> text.md
+echo "markdown:" >> text.md
+echo "  auto_url_links: false" >> text.md
+echo "---" >> text.md
+echo "" >> text.md
+echo "[bibtexify hideMissing=true]" >> text.md
+echo "" >> text.md
+
+export IFS=$'\n'
+for i in `cat ~/Documents/Archivio/Curriculum/pubblicazioni.bib`
+do
+    if [[ "$i" =~ "author"* ]]
+    then
+	AUTHORS=$i
+	FIRSTAUTHOR=`echo $AUTHORS | awk 'BEGIN { FS=" and " } { print $1 }'`
+	if [[ "$FIRSTAUTHOR" == "$AUTHORS" ]]
+	then
+	    echo "$AUTHORS" >> text.md
+	elif [[ "$FIRSTAUTHOR" == *"Duranti"* ]]
+	then
+	    echo "$FIRSTAUTHOR and {et al.}}," >> text.md
+	else
+	    echo "$FIRSTAUTHOR and {...} and {M. Duranti} and {et al.}}," >> text.md
+	fi
+    else
+	echo "$i" >> text.md
+    fi
+done
+
+echo "" >> text.md
+echo "[/bibtexify]" >> text.md
+
+mv -v text.md user/pages/02.publications/02._publications/
