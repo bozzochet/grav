feb1c34f903b0f6afcd7a4107c1c36d0219f3292
Matteo Duranti <bozzo1983@gmail.com>
2021-05-10 11:44:17 +0200

    up

--- update_bibtex.sh
+++ update_bibtex.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#SHORTEN_AUTHORS=0
+SHORTEN_AUTHORS=1
+
 echo "---" > text.md
 echo "title: Publications" >> text.md
 echo "body_classes: modular" >> text.md
@@ -12,26 +15,31 @@ echo "" >> text.md
 echo "[bibtexify hideMissing=true]" >> text.md
 echo "" >> text.md
 
-export IFS=$'\n'
-for i in `cat ~/Documents/Archivio/Curriculum/pubblicazioni.bib`
-do
-    if [[ "$i" =~ "author"* ]]
-    then
-	AUTHORS=$i
-	FIRSTAUTHOR=`echo $AUTHORS | awk 'BEGIN { FS=" and " } { print $1 }'`
-	if [[ "$FIRSTAUTHOR" == "$AUTHORS" ]]
+if [[ "$SHORTEN_AUTHORS" == "1" ]]
+then
+    export IFS=$'\n'
+    for i in `cat ~/Documents/Archivio/Curriculum/pubblicazioni.bib`
+    do
+	if [[ "$i" =~ "author"* ]]
 	then
-	    echo "$AUTHORS" >> text.md
-	elif [[ "$FIRSTAUTHOR" == *"Duranti"* ]]
-	then
-	    echo "$FIRSTAUTHOR and {et al.}}," >> text.md
+	    AUTHORS=$i
+	    FIRSTAUTHOR=`echo $AUTHORS | awk 'BEGIN { FS=" and " } { print $1 }'`
+	    if [[ "$FIRSTAUTHOR" == "$AUTHORS" ]]
+	    then
+		echo "$AUTHORS" >> text.md
+	    elif [[ "$FIRSTAUTHOR" == *"Duranti"* ]]
+	    then
+		echo "$FIRSTAUTHOR and {et al.}}," >> text.md
+	    else
+		echo "$FIRSTAUTHOR and {...} and {M. Duranti} and {et al.}}," >> text.md
+	    fi
 	else
-	    echo "$FIRSTAUTHOR and {...} and {M. Duranti} and {et al.}}," >> text.md
+	    echo "$i" >> text.md
 	fi
-    else
-	echo "$i" >> text.md
-    fi
-done
+    done
+else
+    cat ~/Documents/Archivio/Curriculum/pubblicazioni.bib >> text.md
+fi
 
 echo "" >> text.md
 echo "[/bibtexify]" >> text.md
